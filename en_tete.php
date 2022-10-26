9ce41f4fa44b0ab0c83b8c670b785a774811e827
ggbaguidi <115180159+ggbaguidi@users.noreply.github.com>
2022-10-26 21:54:20 +0100

    Add files via upload
    project starting

---
+++ en_tete.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="en_tete.css">
+    <title></title>
+</head>
+<body>
+    <header>
+        <img src="lab.png" alt="" width=100px heigh=50px>
+        <ul>
+            <li>HOME</li>
+            <li>PUBLICATIONS</li>
+            <li>CHALLENGE</li>
+            <li>CONTACTS</li>
+        </ul>
+    </header>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>revu scientifiques</title>
+</head>
+<body>
+    <?php include("en_tete.php"); ?>
+    <section>
+        <h1>Bienvenue dans l'univers des sciences</h1>
+        <div class="section_accueil">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio at recusandae cumque sapiente officiis, fuga eligendi soluta consectetur doloremque iure. Deleniti dicta qui odit unde, amet asperiores hic? Eum, consequatur?
+            In odio cupiditate magnam nostrum delectus eos, iure saepe quo molestiae blanditiis perferendis quisquam debitis, harum reiciendis facilis eveniet fugit quod! Excepturi, voluptatum. Autem facilis sequi reiciendis. Ipsa, quas saepe.
+            Voluptatem quo nisi eius! Adipisci, aliquid. Neque laborum nulla atque sunt ea asperiores non, odio error laboriosam illum dolorum ex. Esse unde voluptatem ratione itaque tempora inventore vitae, distinctio commodi!
+            Quidem, ratione tempora in soluta quaerat, asperiores nulla laborum modi eligendi quam odit dolores mollitia doloribus veritatis! Debitis quis, numquam ipsum quasi unde, nostrum vero sit non, harum perferendis quaerat!
+            Ipsam laborum fugit earum iure. Ipsam in debitis dolor perferendis, amet aspernatur, explicabo modi doloribus nobis atque excepturi vero a. Iusto nulla voluptatum iste autem adipisci recusandae ipsa aliquam mollitia.
+            Aliquam expedita pariatur, voluptatum numquam tempora delectus eius laboriosam voluptas eaque ab assumenda ratione, omnis repudiandae nostrum facere quod? Recusandae, vero deleniti! Repellendus hic corporis optio, id consequatur at porro.
+            Quod earum aliquid reprehenderit quibusdam debitis nesciunt est! Animi ratione, quas at fugiat debitis explicabo. Aperiam ratione fuga incidunt exercitationem quidem nihil est libero illo voluptate deserunt? Cupiditate, iusto voluptatibus.
+            Sit possimus, temporibus incidunt eveniet praesentium alias corporis fugiat ex, laborum debitis doloribus corrupti officiis, accusantium sapiente ad reprehenderit? Saepe dolore fugiat odio eum ipsum provident molestias dicta. Iste, quia.
+            Maiores, quod officia vel possimus inventore quibusdam neque odit necessitatibus nobis accusantium, iure hic dolorem ullam maxime enim at aperiam beatae expedita unde asperiores! Amet, deleniti obcaecati. Beatae, nam nostrum.
+            Tempora quam nostrum itaque, distinctio alias asperiores maxime quis pariatur minus deserunt ab dolorum ipsa, modi qui iusto rerum, soluta harum mollitia? Dolor ullam nam earum nihil ipsum facere molestias.
+        </div>
+    </section>
+</body>
+</html>
